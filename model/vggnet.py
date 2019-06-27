2e73c57f73f46b684c10c442cd6caf633879a994
anthony0727 <jwseok0727@gmail.com>
2019-06-27 16:46:29 +0900

    generate vggnet by loop. w/ loop, u can build various VGG(VGG11, VGG13...) with small modification of code

--- model/vggnet.py
+++ model/vggnet.py
@@ -9,9 +9,25 @@ import os
 from ABCNet import Network
 from data import Cifar10
 
+N_VGGBLOCK = 5
 
-def conv(input_xs, units, k, s, padding, activation, name):
-    layer = tf.layers.Conv2D(units, k, s, padding, activation=activation, name=name)(input_xs)
+vgg11_config = [-1, 1, 1, 2, 2, 2]
+vgg13_config = [-1, 2, 2, 2, 2, 2]
+vgg16_config = [-1, 2, 2, 3, 3, 3]
+vgg19_config = [-1, 2, 2, 4, 4, 4]
+
+vgg_config = {
+    11: vgg11_config,
+    13: vgg13_config,
+    16: vgg16_config,
+    19: vgg19_config
+}
+
+filters_config = [-1, 64, 128, 256, 512, 512]
+
+
+def conv(prev_layer, units, name):
+    layer = tf.layers.Conv2D(units, (3, 3), (1, 1), 'SAME', activation=tf.nn.relu, name=name)(prev_layer)
     return layer
 
 
@@ -19,21 +35,17 @@ def fc(flat_layer, units, activation, initializer, layer_name):
     layer = tf.layers.Dense(units, activation, kernel_initializer=initializer, name=layer_name)(flat_layer)
     return layer
 
-
-def vgg_block(i, filters, layer):
-    with tf.variable_scope('VGGBLOCK-{}'.format(i)):
-        layer = tf.layers.Conv2D(filters, (3, 3), (1, 1), 'SAME', activation=tf.nn.relu, name='conv1')(layer)
-        layer = tf.layers.Conv2D(filters, (3, 3), (1, 1), 'SAME', activation=tf.nn.relu, name='conv2')(layer)
-    layer = tf.layers.MaxPooling2D((2, 2), (2, 2), name='MaxPool-{}'.format(i))(layer)
-
-    return layer
-
-
 class VGGNet(Network):
-    def __init__(self):
+    def __init__(self, n_layer=11):
         super(VGGNet, self).__init__()
+
         self.input_shape = None
-        self.num_class = 0
+        self.n_class = 0
+
+        if n_layer not in vgg_config.keys():
+            raise ValueError("Unrecognizable VGGNet. Only VGG11, VGG13, VGG16, VGG19 are available")
+
+        self.n_layer = n_layer
 
     def transfer(self, network):
         pass
@@ -48,22 +60,22 @@ class VGGNet(Network):
 
     def attach_layers(self):
         with self.graph.as_default():
-            with tf.variable_scope('VGGBLOCK-1'):
-                layer = tf.layers.Conv2D(32, (3, 3), (1, 1), 'SAME', activation=tf.nn.relu, name='conv1')(self.xs)
-            with tf.variable_scope('VGGBLOCK-2'):
-                layer = tf.layers.Conv2D(64, (3, 3), (1, 1), 'SAME', activation=tf.nn.relu, name='conv1')(layer)
+            layer = self.xs
 
-            # 중복 코드 도저히 못참겠다...
-            filter_config = [128, 256, 256]
-            for i in range(3):
-                block_num = i+3
-                layer = vgg_block(block_num, filter_config[i], layer)
+            blocks = vgg_config[self.n_layer]
+            for ith_block in range(1, N_VGGBLOCK + 1):
+                with tf.variable_scope('VGGBLOCK-{}'.format(ith_block)):
+                    block = blocks[ith_block]
+                    for ith_layer in range(1, block + 1):
+                        filters = filters_config[ith_block]
+                        layer = conv(layer, filters, name='conv{}'.format(ith_layer))
+                    layer = tf.layers.MaxPooling2D((2, 2), (2, 2), name='MaxPool-{}'.format(ith_block))(layer)
 
             with tf.variable_scope('FC'):
                 layer = tf.layers.Flatten()(layer)
                 layer = tf.layers.Dense(1024, activation=tf.nn.relu)(layer)
                 layer = tf.layers.Dropout(0.5)(layer, training=self.is_train)
-                logits = tf.layers.Dense(self.num_class)(layer)
+                logits = tf.layers.Dense(self.n_class)(layer)
 
             self.logits = tf.identity(logits, name='logits')
             pred = tf.nn.softmax(logits, name='predictions')
@@ -73,7 +85,7 @@ class VGGNet(Network):
             l2_reg = tf.reduce_sum([tf.nn.l2_loss(var) for var in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)])
             l2_beta = 0.01
 
-            loss = tf.nn.softmax_cross_entropy_with_logits_v2(self.ys, self.logits)
+            loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=self.ys, logits=self.logits)
             loss = tf.reduce_mean(loss) + (l2_beta * l2_reg)
 
             self.loss = tf.identity(loss, 'loss')
@@ -89,3 +101,7 @@ class VGGNet(Network):
             tf.summary.scalar('accuracy', self.accuracy)
             tf.summary.scalar('loss', self.loss)
             tf.summary.merge_all(name='merge_all')
+
+
+net = VGGNet(19)
+net.build((32, 32, 3), 10)
